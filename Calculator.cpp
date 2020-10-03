e5b1a5124d8d4f33fa96ba177a17746b56709c65
ArghyaKamalGuha-1 <69950559+ArghyaKamalGuha-1@users.noreply.github.com>
2020-10-03 13:20:55 +0530

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     char oper;
     float num1, num2;
-    cout << "Enter an operator (+, -, *, /): ";
+    cout << "Enter an operator (+, -, *, /, %): ";
     cin >> oper;
     cout << "Enter two numbers: " << endl;
     cin >> num1 >> num2;
@@ -23,8 +23,11 @@ int main() {
         case '/':
             cout << num1 << " / " << num2 << " = " << num1 / num2;
             break;
+        case '%':
+            cout << num1<< " % " << num2 << " = " << num1 % num2;
+            break;
         default:
-            // operator is doesn't match any case constant (+, -, *, /)
+            // operator is doesn't match any case constant (+, -, *, /, %)
             cout << "Error! The operator is not correct";
             break;
     }
